64cad15d016211889022e8763147cbd1339dfcc2
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-10 16:05:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -211,9 +211,10 @@ def most_points_scored
   game_hash.each do |place, team|
       team[:players].each do |player_hash|
         array << player_hash[:points]
+      end
         if sorted_array[-1] == player_hash[:points]
          return player_hash[:player_name]
-       end
+       
     end
   end
 end
